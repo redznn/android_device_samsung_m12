0190c416ea9fe2b2190716486e0c77c4b91f10da
redznn <103375250+redznn@users.noreply.github.com>
2022-11-21 19:17:06 -0300

    Initial support for Orange Fox

---
+++ vendorsetup.sh
@@ -0,0 +1,60 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="m12"
+#set -o xtrace
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then 
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	export DEVICE_PATH=device/samsung/m12
+	export LC_ALL="C"
+  
+  #OFR build settings & info
+  export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+  export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/by-name/recovery"
+  export FOX_RECOVERY_BOOT_PARTITION"/dev/block/by-name/boot"
+  export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+  export FOX_DRASTIC_SIZE_REDUCTION=1
+  export OF_FBE_METADATA_MOUNT_IGNORE=1
+  export FOX_REMOVE_AAPT=1
+  
+  	lunch twrp_$FDEVICE-eng
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+fi
+#
+ 	
